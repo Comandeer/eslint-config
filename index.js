f8466b02bf2c6f38e36a0f10e487b4b62fcefa40
Comandeer <vepomoc@gmail.com>
2019-06-29 01:03:30 +0200

    style(index): sort rules alphabetically

--- index.js
+++ index.js
@@ -14,9 +14,53 @@ module.exports = {
 		}
 	},
 	rules: {
+		'array-bracket-spacing': [ 'error', 'always' ],
+		'array-callback-return': 'error',
+		'arrow-body-style': [ 'error', 'always' ],
+		'arrow-parens': [ 'error', 'always' ],
+		'arrow-spacing': [ 'error', {
+			after: true,
+			before: true
+		} ],
+		'block-spacing': 'error',
+		'brace-style': [ 'error', '1tbs' ],
+		camelcase: [ 'error', {
+			properties: 'always'
+		} ],
+		'comma-dangle': [ 'error', 'never' ],
+		'comma-spacing': [ 'error', {
+			after: true,
+			before: false
+		} ],
+		'comma-style': [ 'error', 'last' ],
+		'computed-property-spacing': [ 'error', 'always' ],
 		'constructor-super': 'error',
+		curly: 'error',
+		'dot-location': [ 'error', 'object' ],
+		'dot-notation': 'error',
+		'eol-last': 'error',
+		'eqeqeq': [ 'error', 'smart' ],
 		'for-direction': 'error',
+		'func-style': [ 'error', 'declaration', {
+			allowArrowFunctions: true
+		} ],
+		'generator-star-spacing': [ 'error', 'after' ],
+		'getter-return': [ 'error', {
+			allowImplicit: true
+		} ],
+		indent: [ 'error', 'tab' ],
+		'key-spacing': [ 'error', {
+			afterColon: true,
+			beforeColon: false,
+			mode: 'strict'
+		} ],
+		'keyword-spacing': 'error',
+		'linebreak-style': [ 'error', 'unix' ],
+		'new-cap': 'error',
+		'new-parens': 'error',
+		'no-array-constructor': 'error',
 		'no-async-promise-executor': 'error',
+		'no-await-in-loop': 'error',
 		'no-case-declarations': 'error',
 		'no-class-assign': 'error',
 		'no-compare-neg-zero': 'error',
@@ -34,26 +78,39 @@ module.exports = {
 		'no-empty-character-class': 'error',
 		'no-empty-pattern': 'error',
 		'no-ex-assign': 'error',
+		'no-extend-native': 'error',
 		'no-extra-boolean-cast': 'error',
 		'no-extra-semi': 'error',
 		'no-fallthrough': 'error',
 		'no-func-assign': 'error',
 		'no-global-assign': 'error',
+		'no-implicit-coercion': [ 'error', {
+			allow: [ '!!' ]
+		} ],
 		'no-inner-declarations': 'error',
 		'no-invalid-regexp': 'error',
 		'no-irregular-whitespace': 'error',
+		'no-lonely-if': 'error',
 		'no-misleading-character-class': 'error',
 		'no-mixed-spaces-and-tabs': 'error',
+		'no-multiple-empty-lines': [ 'error', {
+			max: 1
+		} ],
+		'no-native-reassign': 'error',
+		'no-new-object': 'error',
 		'no-new-symbol': 'error',
 		'no-obj-calls': 'error',
 		'no-octal': 'error',
+		'no-proto': 'error',
 		'no-prototype-builtins': 'error',
 		'no-redeclare': 'error',
 		'no-regex-spaces': 'error',
+		'no-return-await': 'error',
 		'no-self-assign': 'error',
 		'no-shadow-restricted-names': 'error',
 		'no-sparse-arrays': 'error',
 		'no-this-before-super': 'error',
+		'no-trailing-spaces': 'error',
 		'no-undef': 'error',
 		'no-unexpected-multiline': 'error',
 		'no-unreachable': 'error',
@@ -61,92 +118,35 @@ module.exports = {
 		'no-unsafe-negation': 'error',
 		'no-unused-labels': 'error',
 		'no-unused-vars': 'error',
-		'no-useless-catch': 'error',
-		'no-useless-escape': 'error',
-		'require-atomic-updates': 'error',
-		'require-yield': 'error',
-		'use-isnan': 'error',
-		'valid-typeof': 'error',
-		indent: [ 'error', 'tab' ],
-		'linebreak-style': [ 'error', 'unix' ],
-		quotes: [ 'error', 'single' ],
-		semi: [ 'error', 'always' ],
-		curly: 'error',
-		'brace-style': [ 'error', '1tbs' ],
-		camelcase: [ 'error', {
-			properties: 'always'
-		} ],
-		'comma-spacing': [ 'error', {
-			before: false,
-			after: true
-		} ],
-		'comma-style': [ 'error', 'last' ],
-		'comma-dangle': [ 'error', 'never' ],
-		'key-spacing': [ 'error', {
-			mode: 'strict',
-			beforeColon: false,
-			afterColon: true
-		} ],
-		'keyword-spacing': 'error',
-		'block-spacing': 'error',
-		'array-bracket-spacing': [ 'error', 'always' ],
-		'object-curly-spacing': [ 'error', 'always' ],
-		'computed-property-spacing': [ 'error', 'always' ],
-		'space-in-parens': [ 'error', 'always' ],
-		'space-before-function-paren': [ 'error', 'never' ],
-		'generator-star-spacing': [ 'error', 'after' ],
-		'yield-star-spacing': [ 'error', 'after' ],
-		'space-infix-ops': 'error',
-		'space-unary-ops': 'error',
-		'spaced-comment': [ 'error', 'always' ],
-		'arrow-spacing': [ 'error', {
-			before: true,
-			after: true
-		} ],
-		'arrow-parens': [ 'error', 'always' ],
-		'arrow-body-style': [ 'error', 'always' ],
-		'no-trailing-spaces': 'error',
-		'no-multiple-empty-lines': [ 'error', {
-			max: 1
-		} ],
-		'eol-last': 'error',
-		'valid-jsdoc': 'error',
-		'no-implicit-coercion': [ 'error', {
-			allow: [ '!!' ]
-		} ],
-		'no-native-reassign': 'error',
-		'no-extend-native': 'error',
-		'wrap-iife': [ 'error', 'outside' ],
-		'yoda': [ 'error', 'never' ],
-		'prefer-arrow-callback': 'error',
-		'prefer-const': 'error',
-		'no-var': 'error',
-		'prefer-template': 'error',
-		'prefer-rest-params': 'error',
-		'prefer-spread': 'error',
-		'object-shorthand': [ 'error', 'always' ],
-		'no-array-constructor': 'error',
-		'no-new-object': 'error',
-		'new-cap': 'error',
-		'new-parens': 'error',
-		'no-lonely-if': 'error',
 		'no-use-before-define': [ 'error', {
 			functions: false
 		} ],
+		'no-useless-catch': 'error',
+		'no-useless-escape': 'error',
+		'no-var': 'error',
 		'no-with': 'error',
-		'eqeqeq': [ 'error', 'smart' ],
-		'getter-return': [ 'error', {
-			allowImplicit: true
-		} ],
-		'no-await-in-loop': 'error',
-		'dot-location': [ 'error', 'object' ],
-		'dot-notation': 'error',
-		'array-callback-return': 'error',
-		'no-proto': 'error',
-		'no-return-await': 'error',
-		'func-style': [ 'error', 'declaration', {
-			allowArrowFunctions: true
-		} ],
-		'require-await': 'error'
+		'object-curly-spacing': [ 'error', 'always' ],
+		'object-shorthand': [ 'error', 'always' ],
+		'prefer-arrow-callback': 'error',
+		'prefer-const': 'error',
+		'prefer-rest-params': 'error',
+		'prefer-spread': 'error',
+		'prefer-template': 'error',
+		quotes: [ 'error', 'single' ],
+		'require-atomic-updates': 'error',
+		'require-await': 'error',
+		'require-yield': 'error',
+		semi: [ 'error', 'always' ],
+		'space-before-function-paren': [ 'error', 'never' ],
+		'space-in-parens': [ 'error', 'always' ],
+		'space-infix-ops': 'error',
+		'space-unary-ops': 'error',
+		'spaced-comment': [ 'error', 'always' ],
+		'use-isnan': 'error',
+		'valid-jsdoc': 'error',
+		'valid-typeof': 'error',
+		'wrap-iife': [ 'error', 'outside' ],
+		'yield-star-spacing': [ 'error', 'after' ],
+		'yoda': [ 'error', 'never' ]
 	}
 };
